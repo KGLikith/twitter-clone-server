6d79ca7c6f7fbe973aa5b33e5b85befaa66be906
KGLikith <glikith1705@gmail.com>
2024-10-10 23:22:00 +0530

    added health route

--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,10 @@ dotenv.config();
 async function init() {
   const app = await initServer();
 
+  app.get('/',(req,res)=>{
+    res.status(200).send({message:"Server is running"})
+  })
+
   app.listen(8000, () => {
     console.log("server started at http://localhost:8000");
   });
