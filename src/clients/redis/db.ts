fca3af339f5b47f315946d94a63c260fc432fc46
KGLikith <glikith1705@gmail.com>
2025-03-06 11:51:28 +0530

    new init

--- src/clients/redis/db.ts
+++ src/clients/redis/db.ts
@@ -1,5 +1,26 @@
-import Redis from "ioredis"
+import Redis from "ioredis";
 
-const redisClient = new Redis(process.env.REDIS_URL || "");
+const redisClient = new Redis(process.env.REDIS_URL || "", {
+  maxRetriesPerRequest: null, // Disables max retry limit
+  reconnectOnError: (err) => {
+    console.error("Redis reconnecting due to error:", err);
+    return true;
+  },
+  retryStrategy: (times) => {
+    if (times > 10) {
+      console.error("Redis retry limit reached");
+      return null; // Stop retrying after 10 attempts
+    }
+    return Math.min(times * 200, 2000); // Exponential backoff
+  },
+});
+
+redisClient.on("error", (err) => {
+  console.error("Redis connection error:", err);
+});
+
+// redisClient.on("connect", () => {
+//   console.log("Connected to Redis successfully!");
+// });
 
 export default redisClient;
